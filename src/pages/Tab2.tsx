eff7fa582c272ae966787e840ec940c3aabd456c
Usuario invitado <Guest@iMac-de-PUCE-A107-09.local>
2026-06-12 16:18:53 -0500

    Laboratorio 7

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonContent, IonHeader, IonInput, IonPage, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
+
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
@@ -7,16 +7,40 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Formulario de repositoriio</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Formulario de repositoriio</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <div className="form-container">
+          <IonInput
+          className="from-field"
+          label="Nombre del Repositorio"
+          labelPlacement="floating"
+          placeholder="Ingrese el nombre del Repsositorio"
+          />
+          <IonTextarea
+          className="from-field"
+          label="Descripcion"
+          labelPlacement="floating"
+          placeholder="Ingrese la descripcion del Repsositorio"
+          rows={6}
+          />
+          <IonButton
+          className= "from-field"
+          expand="block"
+          color="primary"
+          
+          >
+            Guardar
+            
+          </IonButton>
+        </div>
+      
       </IonContent>
     </IonPage>
   );
