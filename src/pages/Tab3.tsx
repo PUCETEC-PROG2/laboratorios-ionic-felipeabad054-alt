2fd679c8b7d4c2ed814fb66a35b1015e1c9181f6
Felipe Abad <felipeabad054@gmail.com>
2026-06-16 17:32:08 -0500

    Laboratorio Finalizado

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCard, IonCardContent, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
@@ -7,16 +7,28 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Perfil</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Perfil</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <div className="card-container">
+          <IonCard className="card">
+            <img src="https://avatars.githubusercontent.com/u/259987936?v=4" alt="Avatar"/>
+            <IonHeader>
+              <IonCardTitle>Felipe Daniel Abad</IonCardTitle>
+              <IonCardSubtitle>felipeabadizurieta</IonCardSubtitle>
+              <IonCardContent>Desarrollador de Sowftware en aplicaciones Web .Apasionado por la tecnologia,el aprendizaje y un poco el trago</IonCardContent>
+            </IonHeader>
+          </IonCard>
+        </div>
+
+
+       
       </IonContent>
     </IonPage>
   );
